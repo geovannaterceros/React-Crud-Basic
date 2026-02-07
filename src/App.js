eb87e9fdf9386b9f0d1ffd51fc4eefed6e0ffa0e
Geovanna <geovannaterceros@gmail.com>
2026-02-06 23:09:15 -0400

    First commit the crud.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+  const [isOn, setIsOn] = useState(false);
+
+  const [entry, setEntry] = useState("");
+
+  const [list, setList] = useState([]);
+
+  const [editingId, setEditingId] = useState(null);
+  const [editingText, setEditingText ] = useState("");
+
+  const [error, setError] = useState("");
+
+  const isSaveDisabled = entry.trim() === "";
+
+
+  const updateItem = (id) => {
+    const updatedList = list.map(item => item.id === id ? { ...item, text: editingText} : item);
+
+    setList(updatedList);
+    setEditingId(null);
+    setEditingText("");
+  };
+  const handleSave = () => {
+    if(entry.trim() === "")
+    {
+      setError("You need enter some text!!");
+      return;//corta la funcion
+    }
+    
+    const newEntry = {id: uuidv4(), text: entry}
+    setList([...list, newEntry]);
+    setEntry("");
+    setError("");
+    
+  };
+
+  const deleteItem = (id) => {
+    //que elementos queremos que se quede, por eso usamos !==
+    const newList = list.filter(item => item.id !== id);
+    setList(newList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>The light is {isOn ? "ON 💡" : "OFF 🌑"}</h1>
+           
+      <h3>Write about your baby's day:</h3>
+      
+      <input 
+        type="text" 
+        placeholder="Example: I fed the baby"
+        value={ entry } // 1. ¿Qué variable va aquí?
+        onChange={(e) => setEntry(e.target.value)} // 2. ¿Qué función actualiza el estado?
+      />
+      {error && <p style={{ color:'red' }}>{error}</p> }
+     
+      <button onClick={handleSave}
+              disabled = {isSaveDisabled}
+      >
+        Save memory
+      </button>
+      <p>Preview of your diary: <strong>{ entry }</strong></p>
+    <ul>
+  {list.map(item => (
+    <li key={item.id}>
+      {editingId === item.id ? (
+        <>
+          <input
+            type="text"
+            value={editingText}
+            onChange={(e) => setEditingText(e.target.value)}
+          />
+          <button onClick={() => updateItem(item.id)}>Save</button>
+        </>
+      ) : (
+        <>
+          {item.text}
+          <button onClick={() => {
+            setEditingId(item.id);
+            setEditingText(item.text);
+          }}>
+            Edit
+          </button>
+          <button onClick={() => deleteItem(item.id)}>X</button>
+        </>
+      )}
+    </li>
+  ))}
+</ul>
     </div>
+
   );
 }
 
